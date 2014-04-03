6d9be0b57884eadd0439e6e7d8dccebac576ad46
Kingo Wang <wanggenguo@outlook.com>
2014-04-04 01:17:40 +0800

    tt

--- maxflow.cpp
+++ maxflow.cpp
@@ -1,75 +1,78 @@
-#include<iostream>
-#include<cstdio>
-#include<queue>
-#include<cstring>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <algorithm>
 using namespace std;
-const int N=20;
-const int INF=0x3f3f3f3f;
-int s[N][N];//记录图的邻接矩阵
-int d[N];//记录图中各点的层次
-int n,m;
-int min(int a,int b)
+#define NUM 202
+#define MAXINT 0x3fffffff
+int flowMap[NUM][NUM];
+int d[NUM];
+vector<int> node[NUM];
+int bfs(int s,int t)
 {
-	return a<b?a:b;
+    //first find a way to t
+    memset((void*)d,-1,sizeof(d));
+    queue<int> q; int now; q.push(s); d[s]=-2; int e;
+    while(!q.empty())
+    {
+        now = q.front();q.pop();
+        for(int i=0;i<node[now].size(); ++i)
+        {
+            e = node[now][i];
+            if(d[e]==-1 && flowMap[now][e]>0)
+            {
+                d[e] = now; q.push(e);
+                if(e==t)break;
+            }
+        }
+    }
+    //no way return 0
+    if(d[t]==-1)return 0;
+    //get the minimum flow on the path
+    int flow = MAXINT;
+    now = t;
+    while(d[now]>=0)
+    {
+        flow = min(flowMap[d[now]][now],flow);
+        now = d[now];
+    }
+    //update the path
+    now = t;
+    while(d[now]>=0)
+    {
+        flowMap[d[now]][now] -= flow;
+        flowMap[now][d[now]] += flow;
+        now = d[now];
+    }
+    return flow;
 }
-bool bfs()
+int maxflow(int s,int t)
 {
-	queue<int>Q;
-	memset(d,-1,sizeof(d));//此处初始化要特别注意，以上版本的初始化就存在很大问题
-	d[1]=0;//如果处理不慎就很容易死循环
-	Q.push(1);
-	while(!Q.empty()){
-		int v=Q.front();Q.pop();
-		for(int i=1;i<=n;i++){
-			if(d[i]==-1&&s[v][i]){//此处应是s[v][i]!=0,而不是以上版本中的s[v][i]>0,因为dfs是可能会走错，这样可以对其进行修正。
-				d[i]=d[v]+1;
-				Q.push(i);
-			}
-		}
-	}
-	return d[n]!=-1;
-}
-int dfs(int v,int cur_flow)
-{
-	int dt=cur_flow;
-	if(v==n)return cur_flow;
-	for(int i=1;i<=n;i++){
-		if(s[v][i]>0&&d[v]+1==d[i]){
-			int flow=dfs(i,min(dt,s[v][i]));
-			s[v][i]-=flow;
-			s[i][v]+=flow;
-			dt-=flow;
-		}
-	}
-	return cur_flow-dt;
-}
-int dinic()
-{
-	int cur_flow,ans=0;
-	while(bfs()){//一次bfs可以找到几条增广路
-		while(cur_flow=dfs(1,INF))
-			ans+=cur_flow;
-	}
-	return ans;
+    int sum=0,u;
+    while(u=bfs(s,t)) { sum += u; }
+    return sum;
 }
 int main()
 {
-	int t,i,cas=0,u,v,w,newF;
-	scanf("%d",&t);
-	while(t--){
-		memset(s,0,sizeof(s));
-		scanf("%d %d",&n,&m);
-		printf("Case %d:\n",++cas);
-		for(i=1;i<=m;i++){
-			scanf("%d %d %d",&u,&v,&w);
-			if(u==v)continue;
-			s[u][v]+=w;
-			s[v][u]+=w;
-			newF = dinic();   //每次执行的话，是在上一次的基础上增加流，而不是完全重新计算，所以newF就是增量
-			if(newF > 0)printf("%d %d\n",i,newF);
-		}
-		//printf("\n");
-	}
-	return 0;
+    int T,n,m,x,y,w,c;
+    cin >> T;
+    for(int caseNumber=1;caseNumber<=T;++caseNumber)
+    {
+        cout << "Case #" << caseNumber << ":\n";
+        cin >> n >> m;
+        for(int i=1;i<=m;++i)
+        {
+            cin >> x >> y >> w;
+            flowMap[x-1][y-1] = w;
+            flowMap[y-1][x-1] = w;
+            node[x-1].push_back(y-1);
+            node[y-1].push_back(x-1);
+            c = maxflow(0,n-1);
+            if(c>0) cout << i << ' ' << c << '\n';
+        }
+        for(int i=0;i<n;++i) node[i].clear();
+    }
+    return 0;
 }
- 
+
